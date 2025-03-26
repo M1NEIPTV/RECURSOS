4a9c1c1fc85ba5cb31eeb6d4add378548c205b5d
M1NEIPTV <132680797+M1NEIPTV@users.noreply.github.com>
2025-03-26 20:57:17 +0100

    Fecha actualizacion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from tenacity import retry, stop_after_attempt, wait_fixed
+from datetime import datetime
 
 # Configuración
 URL = "https://proxy.zeronet.dev/1H3KoazXt2gCJgeD8673eFvQYXG7cbRddU/lista-ace.m3u"
@@ -13,7 +14,7 @@ ESPERA_ENTRE_INTENTOS = 5
 # Descarga la lista con política de reintentos
 @retry(stop=stop_after_attempt(INTENTOS_MAXIMOS), wait=wait_fixed(ESPERA_ENTRE_INTENTOS))
 def descargar_archivo(url):
-    print(f"Descargando archivo desde {url}...")
+    print(f"Descargando archivo desde {url}")
     response = requests.get(url, timeout=10)  # Timeout para evitar esperas indefinidas
     response.raise_for_status()  # Lanza error si la descarga falla
     return response.text
@@ -24,9 +25,16 @@ def modificar_contenido(contenido, reemplazos):
         contenido = contenido.replace(reemplazo, "")
     return contenido
 
+# Añade un canal en la primera línea con la hora de generación del archivo
+def agregar_hora_como_canal(contenido):
+    fecha_actual = datetime.now().strftime("%d/%m/%Y")  # Formato DD/MM/YYYY
+    hora_actual = datetime.now().strftime("%H:%M:%S")   # Formato HH:MM:SS
+    canal_hora = f'#EXTINF:-1 tvg-logo="https://www.dl.dropboxusercontent.com/s/11sa5eu1urweo3e/Actualizado.png", {fecha_actual} {hora_actual}\nhttp://127.0.0.1:6878/ace/getstream?id=\n\n'
+    contenido = canal_hora + contenido
+    return contenido
+
 # Guarda el contenido en un archivo
 def guardar_archivo(nombre_archivo, contenido):
-    
     try:
         with open(nombre_archivo, "w", encoding="utf-8") as archivo:
             archivo.write(contenido)
@@ -37,9 +45,11 @@ def guardar_archivo(nombre_archivo, contenido):
 # Función principal que coordina la ejecución del script
 def main():
     contenido = descargar_archivo(URL)
+    
     if contenido:
         contenido_modificado = modificar_contenido(contenido, REEMPLAZOS)
-        guardar_archivo(ARCHIVO_SALIDA, contenido_modificado)
+        contenido_final = agregar_hora_como_canal(contenido_modificado)
+        guardar_archivo(ARCHIVO_SALIDA, contenido_final)
 
 if __name__ == "__main__":
     main()
